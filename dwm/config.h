ce44c88c4e0835c52dedc7d9fb62af95c8d003da
greentea135 <178342624+greentea135@users.noreply.github.com>
2025-03-16 13:37:12 +0800

    border

--- dwm/config.h
+++ dwm/config.h
@@ -15,8 +15,8 @@ static const char col_gray4[]       = "#FFFFFF";
 static const char col_cyan[]        = "#000000";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm] = { col_gray3, col_gray1, col_gray3 },
+	[SchemeSel]  = { col_gray4, col_cyan,  col_gray3  },
 };
 
 /* tagging */
